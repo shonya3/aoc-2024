d436948c524a43f5ba63f7f6f9b025b19c981e46
shonya3 <poeshonya3@gmail.com>
2024-12-16 06:43:26 +0300

    d7 p1

---
+++ src/bin/07/equation.rs
@@ -0,0 +1,103 @@
+use crate::operation::Operation;
+use std::{num::ParseIntError, str::FromStr};
+
+pub fn parse_equations(input: &str) -> Result<Vec<Equation>, ParseEquationError> {
+    input.lines().map(|s| s.parse()).collect()
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Equation {
+    pub test_value: u64,
+    pub values: Vec<u64>,
+}
+
+impl Equation {
+    pub fn is_possible(&self) -> bool {
+        crate::operation::generate_combinations(self.values.len() - 1)
+            .iter()
+            .any(|operations| self.test_value == self.eval(operations))
+    }
+
+    pub fn eval(&self, operations: &[Operation]) -> u64 {
+        let Some(mut result) = self.values.first().copied() else {
+            return 0;
+        };
+
+        for (i, op) in operations.iter().enumerate() {
+            let Some(next_number) = self.values.get(i + 1) else {
+                break;
+            };
+
+            match op {
+                Operation::Add => result += next_number,
+                Operation::Mul => result *= next_number,
+            }
+        }
+
+        result
+    }
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+pub enum ParseEquationError {
+    NoColon(String),
+    ParseInt(ParseIntError, String),
+}
+
+impl FromStr for Equation {
+    type Err = ParseEquationError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let Some((test_value, values_str)) = s.split_once(":") else {
+            return Err(ParseEquationError::NoColon(s.to_owned()));
+        };
+
+        println!("test_vlaue: {test_value} values_str: {values_str}");
+
+        let test_value = test_value
+            .parse::<u64>()
+            .map_err(|err| ParseEquationError::ParseInt(err, test_value.to_owned()))?;
+
+        let values = values_str
+            .trim()
+            .split(" ")
+            .map(|s| {
+                s.parse::<u64>()
+                    .map_err(|err| ParseEquationError::ParseInt(err, s.to_owned()))
+            })
+            .collect::<Result<Vec<u64>, ParseEquationError>>()?;
+
+        Ok(Equation { test_value, values })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::equation::Equation;
+
+    #[test]
+    fn is_possible() {
+        let p = |s: &str| -> Equation { s.parse().unwrap() };
+
+        assert!(p("190: 10 19").is_possible());
+        assert!(p("3267: 81 40 27").is_possible());
+        assert!(!p("83: 17 5").is_possible());
+        assert!(!p("156: 15 6").is_possible());
+        assert!(!p("7290: 6 8 6 15").is_possible());
+        assert!(!p("161011: 16 10 13").is_possible());
+        assert!(!p("192: 17 8 14").is_possible());
+        assert!(p("292: 11 6 16 20").is_possible());
+    }
+
+    #[test]
+    fn parse() {
+        assert_eq!(
+            Equation {
+                test_value: 190,
+                values: vec![10, 19]
+            },
+            "190: 10 19".parse::<Equation>().unwrap()
+        );
+    }
+}
---
+++ src/bin/07/operation.rs
@@ -0,0 +1,26 @@
+#[derive(Debug, Clone, Copy)]
+pub enum Operation {
+    Add,
+    Mul,
+}
+
+pub fn generate_combinations(n: usize) -> Vec<Vec<Operation>> {
+    let mut out: Vec<Vec<Operation>> = Vec::new();
+
+    if n == 0 {
+        out.push(vec![]);
+        return out;
+    };
+
+    generate_combinations(n - 1).iter().for_each(|combo| {
+        let mut add_combo = combo.clone();
+        add_combo.push(Operation::Add);
+        out.push(add_combo);
+
+        let mut mul_combo = combo.clone();
+        mul_combo.push(Operation::Mul);
+        out.push(mul_combo);
+    });
+
+    out
+}
