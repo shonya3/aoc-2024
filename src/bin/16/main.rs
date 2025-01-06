f95c71c3817ff361b1bb0fc57332e4312ef95cb2
shonya3 <poeshonya3@gmail.com>
2025-01-06 07:49:45 +0300

    compare visited entries by score

--- src/bin/16/main.rs
+++ src/bin/16/main.rs
@@ -1,10 +1,32 @@
+use direction::Direction;
+use map::Map;
+use walker::Solution;
+
 pub mod direction;
 pub mod map;
 pub mod position;
 pub mod walker;
 
 fn main() {
-    let input = std::fs::read_to_string("./files/15.txt").unwrap();
+    let input = std::fs::read_to_string("./files/16.txt").unwrap();
 
-    println!("Day 15");
+    println!("Day 16");
+    println!("Part 1: {}", part1(&input));
+}
+
+pub fn part1(input: &str) -> u32 {
+    let map: Map = input.parse().unwrap();
+    let start = map.find_start_position().unwrap();
+    let solution = Solution {
+        position: start,
+        map: &map,
+        start,
+        moves: vec![],
+        direction: Direction::Right,
+    };
+
+    let complete_solutions = solution.explore_solutions();
+    let min = complete_solutions.iter().map(|s| s.score()).min().unwrap();
+
+    min
 }
--- src/bin/16/walker.rs
+++ src/bin/16/walker.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{HashSet, VecDeque},
-    fmt::Write,
-};
+use std::collections::{HashMap, VecDeque};
 
 use crate::{
     direction::{self, Direction, Rotation},
@@ -30,172 +27,62 @@ impl Solution<'_> {
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct PrintMap(pub Vec<Vec<PrintMapElement>>);
-
-impl std::fmt::Display for PrintMap {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let last_row_i = self.0.iter().len() - 1;
-        for (i, row) in self.0.iter().enumerate() {
-            for el in row.iter() {
-                write!(f, "{el}")?;
-            }
-
-            if i != last_row_i {
-                f.write_char('\n')?
-            };
-        }
-
-        Ok(())
-    }
-}
-
-impl From<Map> for PrintMap {
-    fn from(value: Map) -> Self {
-        PrintMap(
-            value
-                .0
-                .into_iter()
-                .map(|row| row.into_iter().map(PrintMapElement::from).collect())
-                .collect(),
-        )
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum PrintMapElement {
-    Empty,
-    Wall,
-    Start,
-    End,
-    Direction(Direction),
-}
-
-impl PrintMapElement {
-    fn as_char(&self) -> char {
-        match self {
-            PrintMapElement::Empty => '.',
-            PrintMapElement::Wall => '#',
-            PrintMapElement::Start => 'S',
-            PrintMapElement::End => 'E',
-            PrintMapElement::Direction(direction) => direction.as_char(),
-        }
-    }
-}
-
-impl std::fmt::Display for PrintMapElement {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_char(self.as_char())
-    }
-}
-
-impl From<Element> for PrintMapElement {
-    fn from(value: Element) -> Self {
-        match value {
-            Element::Empty => PrintMapElement::Empty,
-            Element::Wall => PrintMapElement::Wall,
-            Element::Start => PrintMapElement::Start,
-            Element::End => PrintMapElement::End,
-        }
-    }
-}
-
-impl std::fmt::Display for Solution<'_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut map = PrintMap::from(self.map.clone());
-
-        let mut pos = self.start;
-        let mut dir = Direction::Right;
-        for movee in &self.moves {
-            match movee {
-                Move::Step(direction) => {
-                    pos = direction::next_position(pos, *direction).unwrap();
-                    map.0[pos.y][pos.x] = PrintMapElement::Direction(dir);
-                }
-                Move::Rotate90Degree(rotation) => {
-                    dir = direction::rotate_90deg(dir, *rotation);
-                }
-            }
-        }
-
-        write!(f, "{map}")
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Visited {
-    pub position: Position,
-    pub direction: Direction,
-    pub a_move: Move,
-}
-
 impl Solution<'_> {
     pub fn explore_solutions(&self) -> Vec<Solution> {
         let mut complete: Vec<Solution> = vec![];
         let mut queue: VecDeque<Solution> = VecDeque::from_iter(vec![self.clone()]);
-        let mut visited: HashSet<Visited> = HashSet::new();
+        let mut visited: HashMap<Visited, u32> = HashMap::new();
 
-        while !queue.is_empty() && complete.len() < 5 {
-            let mut solution = queue.pop_front().unwrap();
+        while let Some(solution) = queue.pop_front() {
+            if let Some(Element::End) = solution.map.get(solution.position) {
+                complete.push(solution.clone());
+                continue;
+            }
 
-            let Some(next_position) =
+            if let Some(next_position) =
                 direction::next_position(solution.position, solution.direction)
-            else {
-                continue;
-            };
-            let Some(el) = solution.map.get(next_position) else {
-                continue;
-            };
+            {
+                if let Some(el) = solution.map.get(next_position) {
+                    if el != Element::Wall {
+                        let mut new_solution = solution.clone();
+                        new_solution.position = next_position;
+                        new_solution.moves.push(Move::Step(new_solution.direction));
+                        let current_score = new_solution.score();
+                        let visited_entry = Visited {
+                            position: new_solution.position,
+                            direction: new_solution.direction,
+                        };
 
-            match el {
-                Element::Empty => {
-                    let mut solution = solution.clone();
-                    solution.position = next_position;
-                    let a_move = Move::Step(solution.direction);
-                    solution.moves.push(a_move);
-
-                    let visited_entry = Visited {
-                        position: solution.position,
-                        direction: solution.direction,
-                        a_move,
-                    };
-
-                    if !visited.contains(&visited_entry) {
-                        queue.push_back(solution);
+                        if visited
+                            .get(&visited_entry)
+                            .map_or(true, |&prev_score| current_score < prev_score)
+                        {
+                            visited.insert(visited_entry, current_score);
+                            queue.push_back(new_solution);
+                        }
                     }
-
-                    visited.insert(visited_entry);
-                }
-                Element::Wall => {}
-                Element::Start => {}
-                Element::End => {
-                    solution.position = next_position;
-                    let a_move = Move::Step(solution.direction);
-                    solution.moves.push(a_move);
-                    complete.push(solution);
-                    continue;
                 }
             }
 
-            direction::ROTATIONS.into_iter().for_each(|rotation| {
-                let next_direction = direction::rotate_90deg(solution.direction, rotation);
-                let mut solution = solution.clone();
-                solution.direction = next_direction;
-                let a_move = Move::Rotate90Degree(rotation);
-                solution.moves.push(a_move);
-
+            for &rotation in direction::ROTATIONS.iter() {
+                let new_direction = direction::rotate_90deg(solution.direction, rotation);
+                let mut new_solution = solution.clone();
+                new_solution.direction = new_direction;
+                new_solution.moves.push(Move::Rotate90Degree(rotation));
+                let current_score = new_solution.score();
                 let visited_entry = Visited {
-                    position: solution.position,
-                    direction: solution.direction,
-                    a_move,
+                    position: new_solution.position,
+                    direction: new_solution.direction,
                 };
 
-                if !visited.contains(&visited_entry) {
-                    queue.push_back(solution);
+                if visited
+                    .get(&visited_entry)
+                    .map_or(true, |&prev_score| current_score < prev_score)
+                {
+                    visited.insert(visited_entry, current_score);
+                    queue.push_back(new_solution);
                 }
-
-                visited.insert(visited_entry);
-            });
+            }
         }
 
         complete
@@ -203,9 +90,123 @@ impl Solution<'_> {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Visited {
+    pub position: Position,
+    pub direction: Direction,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Move {
     Step(Direction),
     Rotate90Degree(Rotation),
+}
+
+impl std::fmt::Display for Solution<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        solution_map::SolutionMap::from(self.clone()).fmt(f)
+    }
+}
+
+mod solution_map {
+    use super::Solution;
+    use crate::{
+        direction::{self, Direction},
+        map::{Element, Map},
+        walker::Move,
+    };
+    use std::fmt::Write;
+
+    #[derive(Debug, Clone)]
+    pub struct SolutionMap(pub Vec<Vec<SolutionMapElement>>);
+
+    impl From<Solution<'_>> for SolutionMap {
+        fn from(solution: Solution) -> Self {
+            let mut map = SolutionMap::from(solution.map.clone());
+
+            let mut pos = solution.start;
+            let mut dir = Direction::Right;
+            for movee in &solution.moves {
+                match movee {
+                    Move::Step(direction) => {
+                        pos = direction::next_position(pos, *direction).unwrap();
+                        map.0[pos.y][pos.x] = SolutionMapElement::Direction(dir);
+                    }
+                    Move::Rotate90Degree(rotation) => {
+                        dir = direction::rotate_90deg(dir, *rotation);
+                    }
+                }
+            }
+
+            map
+        }
+    }
+
+    impl std::fmt::Display for SolutionMap {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let last_row_i = self.0.iter().len() - 1;
+            for (i, row) in self.0.iter().enumerate() {
+                for el in row.iter() {
+                    write!(f, "{el}")?;
+                }
+
+                if i != last_row_i {
+                    f.write_char('\n')?
+                };
+            }
+
+            Ok(())
+        }
+    }
+
+    impl From<Map> for SolutionMap {
+        fn from(value: Map) -> Self {
+            SolutionMap(
+                value
+                    .0
+                    .into_iter()
+                    .map(|row| row.into_iter().map(SolutionMapElement::from).collect())
+                    .collect(),
+            )
+        }
+    }
+
+    #[derive(Debug, Clone, Copy, PartialEq)]
+    pub enum SolutionMapElement {
+        Empty,
+        Wall,
+        Start,
+        End,
+        Direction(Direction),
+    }
+
+    impl SolutionMapElement {
+        fn as_char(&self) -> char {
+            match self {
+                SolutionMapElement::Empty => '.',
+                SolutionMapElement::Wall => '#',
+                SolutionMapElement::Start => 'S',
+                SolutionMapElement::End => 'E',
+                SolutionMapElement::Direction(direction) => direction.as_char(),
+            }
+        }
+    }
+
+    impl std::fmt::Display for SolutionMapElement {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.write_char(self.as_char())
+        }
+    }
+
+    impl From<Element> for SolutionMapElement {
+        fn from(value: Element) -> Self {
+            match value {
+                Element::Empty => SolutionMapElement::Empty,
+                Element::Wall => SolutionMapElement::Wall,
+                Element::Start => SolutionMapElement::Start,
+                Element::End => SolutionMapElement::End,
+            }
+        }
+    }
 }
 
 #[cfg(test)]
